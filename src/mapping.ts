8d997a0893a8b52c6da52ac85b479dba32ef85fc
madumas <madumas@madumas.ca>
2021-01-04 21:36:49 -0500

    catch empty wat

--- src/mapping.ts
+++ src/mapping.ts
@@ -10,8 +10,14 @@ export function handleLogValue(event: LogValue): void {
   let price = new Price(event.address.toHexString() + "-" + event.transaction.hash.toHexString());
   price.address = event.address;
   price.source = contract.src();
+
   let medianizerContract = Medianizer.bind(Address.fromString(price.source.toHexString()));
-  price.name = medianizerContract.wat().toString();
-  price.curValue = BigDecimal.fromString(event.params.val.toString());
-  price.save();
+  let callResult = medianizerContract.try_wat();
+  if (callResult.reverted) {
+    log.info("wat() reverted", [])
+  } else {
+    price.name = medianizerContract.wat().toString();
+    price.curValue = BigDecimal.fromString(event.params.val.toString());
+    price.save();
+  }
 }
