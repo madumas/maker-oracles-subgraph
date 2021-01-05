c33dc657f6201861d78c6597a03c3ed245f4a676
madumas <madumas@madumas.ca>
2021-01-04 23:32:52 -0500

    add medianizer

--- src/medianizerMapping.ts
+++ src/medianizerMapping.ts
@@ -2,22 +2,16 @@ import { log } from '@graphprotocol/graph-ts'
 import { Address, BigInt, BigDecimal } from '@graphprotocol/graph-ts'
 import { LogValue, OSM } from '../generated/MakerOSM/OSM'
 import { Medianizer } from '../generated/MakerOSM/Medianizer'
-import { Price } from '../generated/schema'
+import { OSMPrice, MedianizerPrice } from '../generated/schema'
 import { bytes, decimal, DEFAULT_DECIMALS, ZERO_ADDRESS } from '@protofire/subgraph-toolkit'
 
-export function handleLogValue(event: LogValue): void {
-  //log.info('event', [event.toString()]);
-  let contract = OSM.bind(Address.fromString(event.address.toHexString()));
-  let price = new Price(event.address.toHexString());
-  price.address = event.address;
-  let checkSource = contract.try_src();
-  if (checkSource.reverted) {
-      log.info("not an OSM", []);
-      return;
-  }
-  price.source = contract.src();
+export function handleMedianPrice(event: LogValue): void {
+  let medianizerContract = Medianizer.bind(Address.fromString(event.address.toHexString()));
+  let price = new MedianizerPrice(event.address.toHexString());
+  price.updatedTimeStamp = event.block.timestamp;
+  price.updatedBlockNumber = event.block.number;
+  price.transactionHash = event.transaction.hash;
 
-  let medianizerContract = Medianizer.bind(Address.fromString(price.source.toHexString()));
   let callResult = medianizerContract.try_wat();
   if (callResult.reverted) {
     log.info("wat() reverted", [])
@@ -27,7 +21,6 @@ export function handleLogValue(event: LogValue): void {
         decimal.ZERO,
         decimal.fromBigInt(bytes.toUnsignedInt(event.params.val), DEFAULT_DECIMALS)
     );
-    //let value = BigInt.fromUnsignedBytes(event.params.val);
     price.curValue = amount;
     price.save();
   }
---
+++ src/osmMapping.ts
@@ -0,0 +1,37 @@
+import { log } from '@graphprotocol/graph-ts'
+import { Address, BigInt, BigDecimal } from '@graphprotocol/graph-ts'
+import { LogValue, OSM } from '../generated/MakerOSM/OSM'
+import { Medianizer } from '../generated/MakerOSM/Medianizer'
+import { OSMPrice, MedianizerPrice } from '../generated/schema'
+import { bytes, decimal, DEFAULT_DECIMALS, ZERO_ADDRESS } from '@protofire/subgraph-toolkit'
+
+export function handleLogValue(event: LogValue): void {
+  //log.info('event', [event.toString()]);
+  let contract = OSM.bind(Address.fromString(event.address.toHexString()));
+  let price = new OSMPrice(event.address.toHexString());
+  price.updatedTimeStamp = event.block.timestamp;
+  price.updatedBlockNumber = event.block.number;
+  price.transactionHash = event.transaction.hash;
+  let checkSource = contract.try_src();
+  if (checkSource.reverted) {
+      log.info("not an OSM", []);
+      return;
+  }
+
+  price.medianizer = contract.src().toHex();
+
+  let medianizerContract = Medianizer.bind(Address.fromString(contract.src().toHexString()));
+  let callResult = medianizerContract.try_wat();
+  if (callResult.reverted) {
+    log.info("wat() reverted", [])
+  } else {
+    price.name = medianizerContract.wat().toString();
+    let amount = decimal.max(
+        decimal.ZERO,
+        decimal.fromBigInt(bytes.toUnsignedInt(event.params.val), DEFAULT_DECIMALS)
+    );
+    //let value = BigInt.fromUnsignedBytes(event.params.val);
+    price.curValue = amount;
+    price.save();
+  }
+}
