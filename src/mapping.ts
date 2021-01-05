08d4013f6650149c8e1b9bbe6e68268cebf7bee2
madumas <madumas@madumas.ca>
2021-01-04 20:46:05 -0500

    initial

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,18 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { log } from '@graphprotocol/graph-ts'
+import { BigDecimal } from '@graphprotocol/graph-ts'
+import { LogValue, OSM } from '../generated/MakerOSM/OSM'
+import { Medianizer } from '../generated/MakerOSM/Medianizer'
+import { Price } from '../generated/schema'
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
-
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleLogValue(event: LogValue): void {
+  log.info(event);
+  let contract = OSM.bind(event.address);
+  let price = new Price(event.address + "-" + event.transaction.hash);
+  price.address = event.address;
+  price.source = contract.src();
+  let medianizerContract = Medianizer.bind(price.source);
+  price.name = medianizerContract.wat();
+  price.curValue = BigDecimal.fromString(event.params.val.toString());
+  price.nextValue = BigDecimal.fromString('0');
+  price.save();
 }
