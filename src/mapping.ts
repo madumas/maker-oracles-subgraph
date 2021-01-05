429d7dd29d50fb352d6accef3694c3aab2ea7358
madumas <madumas@madumas.ca>
2021-01-04 21:21:50 -0500

    fix casts

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,11 +6,11 @@ import { Price } from '../generated/schema'
 
 export function handleLogValue(event: LogValue): void {
   //log.info('event', [event.toString()]);
-  let contract = OSM.bind(Address.fromString(event.address.toString()));
-  let price = new Price(event.address.toString() + "-" + event.transaction.hash.toString());
+  let contract = OSM.bind(Address.fromString(event.address.toHexString()));
+  let price = new Price(event.address.toHexString() + "-" + event.transaction.hash.toHexString());
   price.address = event.address;
   price.source = contract.src();
-  let medianizerContract = Medianizer.bind(Address.fromString(price.source.toString()));
+  let medianizerContract = Medianizer.bind(Address.fromString(price.source.toHexString()));
   price.name = medianizerContract.wat().toString();
   price.curValue = BigDecimal.fromString(event.params.val.toString());
   price.save();
